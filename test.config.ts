5640c89651b1ba34c25ec84c744201dc0574a148
Yuvaraj <yuvajothi12@gmail.com>
2026-03-06 11:44:16 +0530

    First Commit

---
+++ test.config.ts
@@ -0,0 +1,17 @@
+export class TestConfig {
+
+    //appUrl = "http://localhost/opencart/upload/"
+    appUrl = "https://naveenautomationlabs.com/opencart"
+    //appUrl = "https://tutorialsninja.com/demo/"
+
+
+    //valid login credentials- create your own login account
+    email = "Rafael_Ruecker@yahoo.com"
+    password = "Kx1YAXlWJDJz2Ag"
+
+
+    //product details
+    productName = "MacBook"
+    productQuantity = "2"
+    totalPrice = "$1,204.00"
+}
